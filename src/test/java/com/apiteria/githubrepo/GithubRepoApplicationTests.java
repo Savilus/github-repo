821629b6ce5783cfe3a96a5130146026bb8877e0
Jakub Łanoszka <jakublanoszka@gmail.com>
2023-08-28 19:14:33 +0200

    start project

---
+++ src/test/java/com/apiteria/githubrepo/GithubRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiteria.githubrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
